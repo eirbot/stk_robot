0f65ce832aa5ba0efe07640a0ba69513331ab15a
Convolutio <79534557+Convolutio@users.noreply.github.com>
2026-06-11 20:21:54 +0200

    feat: working unit test with the 5 vTasks
    this test run the vtasks of the com and the actuators,
    wait 5 seconds and timeout each of them

--- embedded/test/test_main.cpp
+++ embedded/test/test_main.cpp
@@ -1,7 +1,12 @@
 #include <Arduino.h>
 #include <unity.h>
+#include "AppState.hpp"
+#include "Arm_Actuator.hpp"
+#include "Communicator.hpp"
 
 String STR_TO_TEST;
+
+static int timeout;
 
 void setUp(void) {
     // set stuff up here
@@ -13,32 +18,22 @@ void tearDown(void) {
     STR_TO_TEST = "";
 }
 
-void test_string_concat(void) {
-    String hello = "Hello, ";
-    String world = "world!";
-    TEST_ASSERT_EQUAL_STRING(STR_TO_TEST.c_str(), (hello + world).c_str());
+void test_small_communication(void) {
+    TEST_ASSERT_EQUAL(1, 1);
 }
 
-void test_string_substring(void) {
-    TEST_ASSERT_EQUAL_STRING("Hello", STR_TO_TEST.substring(0, 5).c_str());
-}
-
-void test_string_index_of(void) {
-    TEST_ASSERT_EQUAL(7, STR_TO_TEST.indexOf('w'));
-}
-
-void test_string_equal_ignore_case(void) {
-    TEST_ASSERT_TRUE(STR_TO_TEST.equalsIgnoreCase("HELLO, WORLD!"));
-}
-
-void test_string_to_upper_case(void) {
-    STR_TO_TEST.toUpperCase();
-    TEST_ASSERT_EQUAL_STRING("HELLO, WORLD!", STR_TO_TEST.c_str());
-}
-
-void test_string_replace(void) {
-    STR_TO_TEST.replace('!', '?');
-    TEST_ASSERT_EQUAL_STRING("Hello, world?", STR_TO_TEST.c_str());
+// The setup that we do not care a lot in this test
+void espSetup() {
+    
+    Wire.begin(21, 22); 
+    Wire.setClock(100000);
+    // TODO: uncomment before match
+    if (!pcf.begin()) {
+      Serial.println("PCF8575 introuvable");
+      while (1);
+    }
+    pinMode(IntEXT, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(IntEXT), IntEXTfct, FALLING);
 }
 
 void setup()
@@ -46,12 +41,16 @@ void setup()
     delay(2000); // service delay
     UNITY_BEGIN();
 
-    RUN_TEST(test_string_concat);
-    RUN_TEST(test_string_substring);
-    RUN_TEST(test_string_index_of);
-    RUN_TEST(test_string_equal_ignore_case);
-    RUN_TEST(test_string_to_upper_case);
-    RUN_TEST(test_string_replace);
+    espSetup();
+
+    ComWithRasp comRasp;
+    comRasp.StartCom();
+    comRasp.StartWorkers(); 
+
+    vTaskDelay(pdMS_TO_TICKS(5000));
+    appState.timeout = true;
+
+    RUN_TEST(test_small_communication);
 
     UNITY_END(); // stop unit testing
 }
