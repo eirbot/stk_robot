903500ba408b5fa47c114572350305b3c721c9dc
Convolutio <79534557+Convolutio@users.noreply.github.com>
2026-06-17 22:04:06 +0200

    draft: test in main actionneur
    this is dirty but, who care

--- embedded/src/main_actionneurs.cpp
+++ embedded/src/main_actionneurs.cpp
@@ -1,30 +1,64 @@
-#include "ActuatorThread.hpp"
-#include "Arm_ActuatorVTask.hpp"
-#include "Communicator.hpp"
+#include "AppState.hpp"
+#include "ArmOrchestrator.hpp"
 
 #define INCLUDE_vTaskDelay 1
 
-void setup() {
+void test_small_communication(void) {
+    
+    vTaskDelay(pdMS_TO_TICKS(5000));
 
-  Arm_ActuatorManager arm_actuator_manager{};
+    Serial.println("Scheduling Commands...");
+    for (int arm_id; arm_id < 4; arm_id++) {
+        Command init_cmd {'I', {arm_id}};
+        Command ascend {'A', {arm_id, arm_id < 2 ? 10 : -10,}};
+        Command descend {'A', {arm_id, arm_id < 2 ? -10 : 10,}};
+        arm_orchestrator.scheduleCommand(init_cmd);
+        arm_orchestrator.scheduleCommand(ascend);
+        arm_orchestrator.scheduleCommand(descend);
+    }
+    Serial.println("Scheduled! Waiting 7 seconds...");
 
-  static ComWithRasp comRasp{std::vector<ActuatorManager *>{&arm_actuator_manager}};
-  Serial.begin(115200);
+    vTaskDelay(pdMS_TO_TICKS(7000));
 
-  Wire.begin(21, 22);
-  Wire.setClock(100000);
+}
 
-  // TODO: uncomment before match
-  if (!pcf.begin()) {
-    Serial.println("PCF8575 introuvable");
-    while (1)
-      ;
-  }
+// The setup that we do not care a lot in this test
+void espSetup() {
+    Wire.begin(21, 22); 
+    Wire.setClock(100000);
+    // TODO: uncomment before match
+    if (!pcf.begin()) {
+      Serial.println("PCF8575 introuvable");
+      while (1);
+    }
+    pinMode(IntEXT, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(IntEXT), IntEXTfct, FALLING);
+}
 
-  pinMode(IntEXT, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(IntEXT), IntEXTfct, FALLING);
+void arm_orchestrator_life(void *pvParameters) {
+    while (!appState.timeout) {
+       arm_orchestrator.loop(); 
+       vTaskDelay(pdMS_TO_TICKS(ARM_TASK_DELAY_MS));
+    }
+}
 
-  comRasp.StartCom();
+void setup()
+{
+    Serial.println("Waiting 2 seconds...");
+    delay(2000); // service delay
+
+    Serial.println("Tests started!");
+
+    espSetup();
+    Serial.println("Start orchestrator task!");
+    xTaskCreatePinnedToCore(arm_orchestrator_life, "armOrchestratorPc", 4000, NULL, 1, NULL, tskNO_AFFINITY);
+
+    Serial.println("Orchestrator task started! Waiting 2seconds");
+    vTaskDelay(pdMS_TO_TICKS(2000));
+
+    test_small_communication();
+
+    appState.timeout = true;
 }
 
 void loop() {}
