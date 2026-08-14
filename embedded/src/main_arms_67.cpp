d74322134bbf371663a09435618cdc6774b73f2e
Convolutio <79534557+Convolutio@users.noreply.github.com>
2026-08-14 19:26:24 +0200

    fix: test in a freertos task

--- embedded/src/main_arms_67.cpp
+++ embedded/src/main_arms_67.cpp
@@ -1,4 +1,8 @@
+#include "ActiveObjectCreator.hpp"
 #include "AppState.hpp"
+#include "Arm.hpp"
+#include "ArmCreator.hpp"
+#include "Arms.hpp"
 #include "Arduino.h"
 #include "Arm_Actuator.hpp"
 #include "freertos/projdefs.h"
@@ -18,37 +22,34 @@ void espSetup() {
   }
 }
 
-void setup()
-{
+void my_arm_task(void *pvParameters) {
+  Arm_Actuator my_arm{ArmActuator1, get_static_pcf(),
+                      armInterruptionStateRef(ArmActuator1)};
+  Serial.println("Orchestrator task started! Waiting 3 seconds");
+  vTaskDelay(pdMS_TO_TICKS(3000));
+  Serial.println("Homming...");
+  my_arm.homming();
+  vTaskDelay(pdMS_TO_TICKS(3000));
+  Serial.println("Elevate the arm...");
+  my_arm.setElevatorPosition(100);
+  vTaskDelay(pdMS_TO_TICKS(3000));
+  Serial.println("Timeout! Every task will be stopped.");
+  appState.timeout = true;
+}
+
+void setup() {
     Serial.begin(115200);
+    Serial.println("Waiting 2 seconds...");
     attachSensorInterruptSignals();
     espSetup();
-    Serial.println("Waiting 2 seconds...");
-
+    FreeRTOSTaskStaticBuffers task_buffer = {armUxCreatorTaskStackBuffer +
+                                                 (0 * ARM_TASK_STACK_SIZE),
+                                             armXCreatorTaskTCBBuffer + 0};
+    xTaskCreateStatic(my_arm_task, "arm1", ARM_TASK_STACK_SIZE, NULL,
+                      ARM_TASK_PRIORITY, task_buffer.puxStackBuffer,
+                      task_buffer.pxTaskBuffer);
     delay(2000); // service delay
-
     Serial.println("Start orchestrator task!");
-
-    Arm_Actuator my_arm{ArmActuator1, get_static_pcf(), armInterruptionStateRef(ArmActuator1)};
-
-    Serial.println("Orchestrator task started! Waiting 3 seconds");
-
-    vTaskDelay(pdMS_TO_TICKS(3000));
-
-    Serial.println("Homming...");
-
-    my_arm.homming();
-
-    vTaskDelay(pdMS_TO_TICKS(3000));
-
-    Serial.println("Elevate the arm...");
-
-    my_arm.setElevatorPosition(100);
-
-    vTaskDelay(pdMS_TO_TICKS(3000));
-
-    Serial.println("Timeout! Every task will be stopped.");
-    appState.timeout = true;
 }
 
 void loop() {}
