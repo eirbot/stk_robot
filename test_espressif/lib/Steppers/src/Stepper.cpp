9d2e44a3f3cf7bd503fe647ad6cbb2481cb6e25f
Quentin Fallito <quentinfallito@gmail.com>
2026-08-15 18:05:47 +0200

    in progress

--- test_espressif/lib/Steppers/src/Stepper.cpp
+++ test_espressif/lib/Steppers/src/Stepper.cpp
@@ -1,38 +1,84 @@
 #include "Stepper.hpp"
 
-Stepper::Stepper(   mcpwm_timer_config_t timer_config,
-                    mcpwm_operator_config_t operator_config,
-                    mcpwm_comparator_config_t comparator_config,
-                    mcpwm_generator_config_t generator_config,
+bool on_reach_function(pcnt_unit_handle_t unit, const pcnt_watch_event_data_t *edata, void *user_ctx){
+    Stepper *stepper = (Stepper *)user_ctx;
+    //TODO
+    
+    return 0;
+}
+
+Stepper::Stepper(   int group_id,
+                    int intr_priority,
+                    int GPIO,
+                    mcpwm_timer_handle_t timer,
+                    mcpwm_oper_handle_t oper,
+                    mcpwm_cmpr_handle_t comparator,
+                    mcpwm_gen_handle_t generator,
                     float gain_step){
+    
+    _timer_config = {
+        .group_id = group_id,
+        .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
+        .resolution_hz = PWM_RESOLUTION,
+        .count_mode = MCPWM_TIMER_COUNT_MODE_UP,
+        .period_ticks = PWM_RESOLUTION/DEFAULT_FREQ,
+    };
+    _operator_config = {
+        .group_id = group_id,
+        .intr_priority = intr_priority
+    };
+    _comparator_config = {
+        .intr_priority = intr_priority
+    };
+    _generator_config = {
+        .gen_gpio_num = GPIO
+    };
+    
+    _timer = timer;
+    _oper = oper;
+    _comparator = comparator;
+    _generator = generator;
+
     _GPIO = GPIO;
-    _MCPWM_UNIT = MCPWM_UNIT;
-    _MCPWM_TIMER = MCPWM_TIMER;
-    _MCPWM_OUTPUT_TIMER_OPERATOR = MCPWM_OUTPUT_TIMER_OPERATOR;    
+    _gain_step = gain_step;
 }
 
 bool Stepper::init(){
-    mcpwm_gpio_init(
-        _MCPWM_UNIT,
-        _MCPWM_OUTPUT_TIMER_OPERATOR,
-        _GPIO
-    );
-    mcpwm_config_t pwm_config = {
-        .frequency = _freq,
-        .cmpr_a = 50.0,
-        .cmpr_b = 0.0,
-        .duty_mode = MCPWM_DUTY_MODE_0,
-        .counter_mode = MCPWM_UP_COUNTER
-    };
-    mcpwm_init(_MCPWM_UNIT, _MCPWM_TIMER, &pwm_config);
+    ESP_ERROR_CHECK(mcpwm_new_timer(&_timer_config,&_timer));
+    ESP_ERROR_CHECK(mcpwm_new_operator(&_operator_config,&_oper));
+    ESP_ERROR_CHECK(mcpwm_operator_connect_timer(_oper, _timer));
+    ESP_ERROR_CHECK(mcpwm_new_comparator(_oper,&_comparator_config,&_comparator));
+    ESP_ERROR_CHECK(mcpwm_new_generator(_oper,&_generator_config,&_generator));
+    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event(_generator,{.direction = MCPWM_TIMER_DIRECTION_UP,.event = MCPWM_TIMER_EVENT_EMPTY,.action = MCPWM_GEN_ACTION_HIGH,}));
+    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(_generator,{.direction = MCPWM_TIMER_DIRECTION_UP,.comparator = _comparator,.action = MCPWM_GEN_ACTION_LOW,}));
+    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(_comparator,_timer_config.period_ticks / 2));
+    
+    pcnt_unit_config_t unit_config = {.clk_src = PCNT_CLK_SRC_DEFAULT, .low_limit = -1, .high_limit = 1000, .intr_priority = 0, .flags = {.accum_count = 1}};
+    ESP_ERROR_CHECK(pcnt_new_unit(&unit_config,&_pcnt));
+    pcnt_chan_config_t chan_config = {.edge_gpio_num = _GPIO, .level_gpio_num = -1, .flags = {.invert_edge_input = 0, .invert_level_input = 0, .virt_edge_io_level = 0, .virt_level_io_level = 0}};
+    ESP_ERROR_CHECK(pcnt_new_channel(_pcnt,&chan_config,&_pcnt_chan));
+
+    ESP_ERROR_CHECK(pcnt_channel_set_edge_action(_pcnt_chan,PCNT_CHANNEL_EDGE_ACTION_INCREASE,PCNT_CHANNEL_EDGE_ACTION_HOLD));
+
+    pcnt_event_callbacks_t my_callbacks{.on_reach = on_reach_function};
+
+    ESP_ERROR_CHECK(pcnt_unit_register_event_callbacks(_pcnt, &my_callbacks, this));
+    ESP_ERROR_CHECK(pcnt_unit_enable(_pcnt));
+    return 0;
 }
 
 bool Stepper::set_frequency(int frequency){
-    mcpwm_set_frequency(_MCPWM_UNIT, _MCPWM_TIMER, frequency);
+    uint32_t new_preriod = PWM_RESOLUTION / frequency;
+    ESP_ERROR_CHECK(mcpwm_timer_set_period(_timer, new_preriod));
     return 0;
 }
 
 int Stepper::set_steps(int steps){
-    int time = steps/_freq; // in s
-    mcpwm_start(_MCPWM_UNIT, _MCPWM_TIMER);
+
+    ESP_ERROR_CHECK(pcnt_unit_add_watch_point(_pcnt,steps));
+    ESP_ERROR_CHECK(pcnt_unit_clear_count(_pcnt));
+    ESP_ERROR_CHECK(pcnt_unit_start(_pcnt));
+
+    return 0;
 }
+
--- test_espressif/lib/Steppers/src/Stepper.hpp
+++ test_espressif/lib/Steppers/src/Stepper.hpp
@@ -2,26 +2,45 @@
 #define STEPPER_HPP
 
 #include "driver/mcpwm_prelude.h"
+#include "driver/pulse_cnt.h"
+
 #define DEFAULT_FREQ 1000 // in Hertz
-#define PWM_RESOLUTION
+#define PWM_RESOLUTION 10000
 class Stepper{
     public:
-        Stepper(mcpwm_timer_config_t timer_config,
-                mcpwm_operator_config_t operator_config,
-                mcpwm_comparator_config_t comparator_config,
-                mcpwm_generator_config_t generator_config,
+        Stepper(int group_id,
+                int intr_priority,
+                int GPIO,
+                mcpwm_timer_handle_t timer,
+                mcpwm_oper_handle_t oper,
+                mcpwm_cmpr_handle_t comparator,
+                mcpwm_gen_handle_t generator,  
                 float gain_step);
         bool init();
         //TODO: define min and max frequency
         bool set_frequency(int frequency); // set frequency e.g the speed of the motor
         int set_steps(int steps); // set number of step for the stepper and returns the time requiered for the steps to be achieved
     private:
+        void stop();
+
         int _freq = DEFAULT_FREQ;
         int _GPIO;
+        float _gain_step;
+
         mcpwm_timer_config_t _timer_config;
         mcpwm_operator_config_t _operator_config;
         mcpwm_comparator_config_t _comparator_config;
         mcpwm_generator_config_t _generator_config;
+
+        mcpwm_timer_handle_t _timer = NULL;
+        mcpwm_oper_handle_t _oper = NULL;
+        mcpwm_cmpr_handle_t _comparator = NULL;
+        mcpwm_gen_handle_t _generator = NULL;
+
+        pcnt_unit_handle_t _pcnt;
+        pcnt_channel_handle_t _pcnt_chan;
+        
+
 
 };
 
