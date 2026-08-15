8a8552534daf18cbbb70eafdca3b73455c8ee7b4
Quentin Fallito <quentinfallito@gmail.com>
2026-08-15 21:07:13 +0200

    i forgor the todo line

--- test_espressif/lib/Steppers/src/Stepper.cpp
+++ test_espressif/lib/Steppers/src/Stepper.cpp
@@ -87,7 +87,6 @@ int Stepper::set_steps(int steps, unsigned int &time_to_wait){
     ESP_ERROR_CHECK(pcnt_unit_clear_count(_pcnt));
     ESP_ERROR_CHECK(pcnt_unit_start(_pcnt));
     ESP_ERROR_CHECK(mcpwm_timer_start_stop(_timer,MCPWM_TIMER_START_NO_STOP));
-    // TODO: return the correct time to be awaited
     time_to_wait = steps/_freq;
 
     return 0;
