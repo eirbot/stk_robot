f236bf4f5d0501169437acb5d20834b90f1b8adf
Convolutio <79534557+Convolutio@users.noreply.github.com>
2026-08-15 20:37:20 +0200

    fix: callback naming

--- test_espressif/lib/Steppers/src/Stepper.cpp
+++ test_espressif/lib/Steppers/src/Stepper.cpp
@@ -1,6 +1,6 @@
 #include "Stepper.hpp"
 
-bool on_reach_function(pcnt_unit_handle_t unit, const pcnt_watch_event_data_t *edata, void *user_ctx){
+bool interrupt_stepper_when_steps_reached(pcnt_unit_handle_t unit, const pcnt_watch_event_data_t *edata, void *user_ctx){
     // TODO: pass the Stepper and call a public method to stop the stepper, the counter and
     Stepper *stepper = (Stepper *)user_ctx;
     stepper->interrupt();
@@ -60,7 +60,7 @@ bool Stepper::init(){
 
     ESP_ERROR_CHECK(pcnt_channel_set_edge_action(_pcnt_chan,PCNT_CHANNEL_EDGE_ACTION_INCREASE,PCNT_CHANNEL_EDGE_ACTION_HOLD));
 
-    pcnt_event_callbacks_t my_callbacks{.on_reach = on_reach_function};
+    pcnt_event_callbacks_t my_callbacks{.on_reach = interrupt_stepper_when_steps_reached};
 
     ESP_ERROR_CHECK(pcnt_unit_register_event_callbacks(_pcnt, &my_callbacks, this));
     ESP_ERROR_CHECK(pcnt_unit_enable(_pcnt));
