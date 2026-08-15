fae5d0b32e856e292d1f523ccb7c059783359fb1
Quentin Fallito <quentinfallito@gmail.com>
2026-08-15 21:09:44 +0200

    inshallla ça passe

--- test_espressif/lib/Steppers/src/Stepper.cpp
+++ test_espressif/lib/Steppers/src/Stepper.cpp
@@ -3,7 +3,6 @@
 bool interrupt_stepper_when_steps_reached(pcnt_unit_handle_t unit, const pcnt_watch_event_data_t *edata, void *user_ctx){
     Stepper *stepper = (Stepper *)user_ctx;
     stepper->interrupt();
-    // TODO: check this variable
     return 0;
 }
 
