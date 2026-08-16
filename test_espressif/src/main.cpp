11a31a92c0a358bdac7d1b5a88d2fc855c791c16
Convolutio <79534557+Convolutio@users.noreply.github.com>
2026-08-16 03:44:37 +0200

    feat: test of two steppers in two freertos tasks

--- test_espressif/src/main.cpp
+++ test_espressif/src/main.cpp
@@ -1,6 +1,7 @@
 #include <esp_log.h>
 #include "Stepper.hpp"
 #include "freertos/FreeRTOS.h"
+#include "freertos/idf_additions.h"
 // int group_id,
 // int intr_priority,
 // int pwmGPIO,
@@ -26,8 +27,8 @@ pcnt_chan_config_t chan_config;
 pcnt_unit_handle_t pcnt_unit;
 pcnt_channel_handle_t pcnt_chan;
 
-int pwmGPIO = 25;
-gpio_num_t dirGPIO = GPIO_NUM_18;
+int pwmGPIO = 18;
+gpio_num_t dirGPIO = GPIO_NUM_25;
 
 Stepper stepper1(0, 1, pwmGPIO, dirGPIO, 1.0, timer, oper, comparator,
                  generator, unit_config, chan_config, pcnt_unit, pcnt_chan,
@@ -53,34 +54,56 @@ Stepper stepper2(0, 1, pwmGPIO_2, dirGPIO_2, 1.0, timer_2, oper_2, comparator_2,
 unsigned int time_to_wait;
 
 // FreeRTOS tasks
-// #define STEPPER_NUMBER 2
-// #define STEPPER_TASK_STACK_SIZE (sizeof(Stepper)) * 4
-// #define STEPPER_TASK_PRIORITY (tskIDLE_PRIORITY + 2)
-// static StaticTask_t xCreatorTaskTCBBuffers[STEPPER_NUMBER];
-// static StackType_t uxCreatorTaskStackBuffers[STEPPER_TASK_STACK_SIZE * STEPPER_NUMBER];
-// static TaskHandle_t tasks[STEPPER_NUMBER];
+#define STEPPER_NUMBER 2
+#define STEPPER_TASK_STACK_SIZE 4096
+#define STEPPER_TASK_PRIORITY (tskIDLE_PRIORITY + 2)
+static StaticTask_t xCreatorTaskTCBBuffers[STEPPER_NUMBER];
+static StackType_t uxCreatorTaskStackBuffers[STEPPER_TASK_STACK_SIZE * STEPPER_NUMBER];
+static TaskHandle_t tasks[STEPPER_NUMBER];
 
 static const char *TAG = "Stepper";
 
+void stepper_one_task(void *pvParameters) {
+    stepper1.init();
+    stepper1.set_frequency(500);
+    
+    while (1) {
+        stepper1.set_steps(500, time_to_wait);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void stepper_two_task(void *pvParameters) {
+    stepper2.init();
+    stepper2.set_frequency(500);
+    
+    while (1) {
+        stepper2.set_steps(200, time_to_wait);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        stepper2.set_steps(-200, time_to_wait);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+    
+}
 extern "C" void app_main() {
     /*SETUP*/
-    stepper1.init();
-    ESP_LOGI(TAG, "Stepper initialized");
-    
+    tasks[0] =
+        xTaskCreateStatic(stepper_one_task, "stepper1", STEPPER_TASK_STACK_SIZE,
+                          NULL, STEPPER_TASK_PRIORITY,
+                          uxCreatorTaskStackBuffers, xCreatorTaskTCBBuffers);
+    tasks[1] =
+        xTaskCreateStatic(stepper_two_task, "stepper2", STEPPER_TASK_STACK_SIZE,
+                          NULL, STEPPER_TASK_PRIORITY,
+                          uxCreatorTaskStackBuffers + STEPPER_TASK_STACK_SIZE,
+                          xCreatorTaskTCBBuffers + 1);
+    ESP_LOGI(TAG, "Steppers initialized");
+
     /*LOOP*/
-    while (true)
-    {
-        stepper1.set_frequency(2000);
-        stepper2.set_frequency(2000);
-        stepper1.set_steps(100, time_to_wait);
-        stepper2.set_steps(100, time_to_wait);
-        vTaskDelay(pdMS_TO_TICKS(500));
-
-        stepper1.set_frequency(1000);
-        stepper2.set_frequency(1000);
-        stepper1.set_steps(-200, time_to_wait);
-        stepper2.set_steps(-200, time_to_wait);
-        vTaskDelay(pdMS_TO_TICKS(500));
-
+    while (1) {
+        ESP_LOGI(TAG, "TIC");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        ESP_LOGI(TAG, "TAC");
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
+
 }
